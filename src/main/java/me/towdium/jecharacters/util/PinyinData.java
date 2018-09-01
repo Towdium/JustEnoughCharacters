8a37011622cee39c862302c272640e4689eec33d
Juntong Liu <towdium@gmail.com>
2018-09-02 00:28:29 +0800

    Fix encoding

--- src/main/java/me/towdium/jecharacters/util/PinyinData.java
+++ src/main/java/me/towdium/jecharacters/util/PinyinData.java
@@ -5,6 +5,7 @@ import me.towdium.jecharacters.core.JechCore;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class PinyinData {
     private static String[][] data;
@@ -15,7 +16,7 @@ public class PinyinData {
         data = new String[41000][];
         String resourceName = "/assets/jecharacters/pinyin.txt";
         BufferedReader br = new BufferedReader(new InputStreamReader(
-                PinyinData.class.getResourceAsStream(resourceName)));
+                PinyinData.class.getResourceAsStream(resourceName), StandardCharsets.UTF_8));
         try {
             String line;
             while ((line = br.readLine()) != null) {
