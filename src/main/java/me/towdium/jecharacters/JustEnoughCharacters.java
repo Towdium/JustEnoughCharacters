71d3fdaaf07f3544946058ba9d3b749268883373
Towdium <towdium@gmail.com>
2021-07-04 22:40:32 +0800

    Fix file version

--- src/main/java/me/towdium/jecharacters/JustEnoughCharacters.java
+++ src/main/java/me/towdium/jecharacters/JustEnoughCharacters.java
@@ -7,15 +7,21 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.ModLoader;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
+import net.minecraftforge.fml.loading.FMLLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
 import static me.towdium.jecharacters.JechConfig.Spell.QUANPIN;
+import static net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus.MOD;
 
-@Mod.EventBusSubscriber
+
+@Mod.EventBusSubscriber(bus = MOD)
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
 @Mod(JustEnoughCharacters.MODID)
@@ -29,19 +35,32 @@ public class JustEnoughCharacters {
     }
 
     @SubscribeEvent
-    public static void onPlayerLogin(EntityJoinWorldEvent event) {
-        if (event.getEntity() instanceof PlayerEntity && event.getEntity().world.isRemote
-                && JechConfig.enableChat.get() && !messageSent
-                && (JechConfig.enumKeyboard.get() == QUANPIN)
-                && Minecraft.getInstance().gameSettings.language.equals("zh_tw")) {
-            printMessage(new TranslationTextComponent("jecharacters.chat.taiwan"));
-            messageSent = true;
+    public static void onConstruct(FMLConstructModEvent event) {
+        if (ModList.get().isLoaded("i18nupdatemod")) {
+            logger.info("Good to see you, TartaricAcid and your friends.");
+        }
+        if (ModList.get().isLoaded("fruittrees") || ModList.get().isLoaded("snowrealmagic")) {
+            logger.info("Good to see you, Snownee.");
         }
     }
 
     @SuppressWarnings("resource")
     public static void printMessage(ITextComponent message) {
         Minecraft.getInstance().ingameGUI.getChatGUI().printChatMessage(message);
+    }
+
+    @Mod.EventBusSubscriber
+    static class EventHandler {
+        @SubscribeEvent
+        public static void onPlayerLogin(EntityJoinWorldEvent event) {
+            if (event.getEntity() instanceof PlayerEntity && event.getEntity().world.isRemote
+                    && JechConfig.enableChat.get() && !messageSent
+                    && (JechConfig.enumKeyboard.get() == QUANPIN)
+                    && Minecraft.getInstance().gameSettings.language.equals("zh_tw")) {
+                printMessage(new TranslationTextComponent("jecharacters.chat.taiwan"));
+                messageSent = true;
+            }
+        }
     }
 }
 
