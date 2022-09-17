9fddbffed0554f7d086dd17cdb945826e5b8bae4
vfyjxf <2331007009@qq.com>
2022-09-17 21:20:04 +0800

    add more mod support

--- src/main/java/me/towdium/jecharacters/JechConfig.java
+++ src/main/java/me/towdium/jecharacters/JechConfig.java
@@ -301,7 +301,10 @@ public class JechConfig {
                             "com.latmod.mods.projectex.gui.GuiTableBase:updateValidItemList",  // Project EX
                             "amerifrance.guideapi.gui.GuiSearch:getMatches",  // Guide-API
                             "thaumcraft.client.gui.GuiResearchBrowser:updateSearch",  // Thaumcraft
-                            "astavie.thermallogistics.client.gui.GuiTerminalItem:updateFilter"  // Thermal Logistics
+                            "astavie.thermallogistics.client.gui.GuiTerminalItem:updateFilter",  // Thermal Logistics
+                            "com.the9grounds.aeadditions.gui.GuiTerminal:updateFluids", // AE Additions Terminal
+                            "com.the9grounds.aeadditions.gui.GuiStorage:updateFluids", // AE Additions Terminal
+                            "me.desht.pneumaticcraft.common.recipes.AmadronOffer:passesQuery" //PneumaticCraft Amadron
                     };
                 case LIST_DEFAULT_REGEXP:
                     return new String[]{
@@ -328,8 +331,12 @@ public class JechConfig {
                     return new String[]{
                             "net.minecraft.client.util.PinyinTree:<init>",  // vanilla search
                             "net.minecraft.client.util.PinyinTree:recalculate",  // vanilla search
+                            "net.minecraft.client.util.SearchTree:<init>", // vanilla search
+                            "net.minecraft.client.util.SearchTree:recalculate", // vanilla search
                             "cgw:<init>",  // vanilla search notch name
                             "cgw:a",  // vanilla search notch name
+                            "cgu:<init>", // vanilla search notch name
+                            "cgu:a", // vanilla search notch name
                             "buildcraft.lib.client.guide.GuideManager:generateContentsPage"  // BuildCraft manual
                     };
                 case LIST_DEFAULT_STRSKT:
