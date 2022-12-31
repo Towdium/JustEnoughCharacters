9fb77a144347a3ecb21c28115818466f9d75cbd7
vfyjxf <2331007009@qq.com>
2022-12-31 20:10:14 +0800

    fix jei support

--- src/main/resources/me/towdium/jecharacters/scripts/jei2.js
+++ src/main/resources/me/towdium/jecharacters/scripts/jei2.js
@@ -1,24 +1,35 @@
 function initializeCoreMod() {
     Java.type('net.minecraftforge.coremod.api.ASMAPI').loadFile('me/towdium/jecharacters/scripts/_lib.js');
     return {
-        'jecharacters-jei2': {
+        'jecharacters-jei2-legacy': {
             'target': {
                 'type': 'METHOD',
                 'class': 'mezz.jei.common.search.ElementPrefixParser',
                 'methodName': '<init>',
                 'methodDesc': '()V'
             },
-            'transformer': function (method) {
-                transInvokeLambda(method,
-                    'mezz/jei/core/search/suffixtree/GeneralizedSuffixTree',
-                    '<init>',
-                    '()V',
-                    'me/towdium/jecharacters/utils/Match$FakeTree',
-                    '<init>',
-                    '()V'
-                );
-                return method;
-            }
+            'transformer': transform
+        },
+        'jecharacters-jei-new': {
+            'target': {
+                'type': 'METHOD',
+                'class': 'mezz.jei.gui.search.ElementPrefixParser',
+                'methodName': '<init>',
+                'methodDesc': '()V'
+            },
+            'transformer': transform
         }
     }
+}
+
+function transform(method) {
+    transInvokeLambda(method,
+        'mezz/jei/core/search/suffixtree/GeneralizedSuffixTree',
+        '<init>',
+        '()V',
+        'me/towdium/jecharacters/utils/Match$FakeTree',
+        '<init>',
+        '()V'
+    );
+    return method;
 }
