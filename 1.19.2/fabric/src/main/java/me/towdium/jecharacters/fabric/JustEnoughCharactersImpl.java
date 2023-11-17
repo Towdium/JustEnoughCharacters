94795a3f9063d592d3f86fee38ff88ce575f81bc
vfyjxf_ <2331007009@qq.com>
2023-11-17 13:04:55 +0800

    clean code

--- 1.19.2/fabric/src/main/java/me/towdium/jecharacters/fabric/JustEnoughCharactersImpl.java
+++
@@ -1,7 +0,0 @@
-package me.towdium.jecharacters.fabric;
-
-public class JustEnoughCharactersImpl {
-    public static String getSuffixClassName() {
-        return "net/minecraft/class_1128";
-    }
-}
--- 1.19.4/forge/src/main/java/me/towdium/jecharacters/forge/JustEnoughCharactersImpl.java
+++
@@ -1,7 +0,0 @@
-package me.towdium.jecharacters.forge;
-
-public class JustEnoughCharactersImpl {
-    public static String getSuffixClassName() {
-        return "net/minecraft/client/searchtree/SuffixArray";
-    }
-}
